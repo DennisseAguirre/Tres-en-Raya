9a78d78f878c6865dc699b19fd5e219984684a88
Juan Pisco <jpisco@espol.edu.ec>
2021-08-06 14:45:10 -0500

    Prueba

---
+++ src/grupo_06/Grupo_06.java
@@ -0,0 +1,17 @@
+
+package grupo_06;
+
+/**
+ *
+ * @author Juan Pisco
+ */
+public class Grupo_06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // prueba
+    }
+    
+}
