a9944dcbae44ee8c1ad17c2d2052830cf6a82c28
Dennisse Aguirre <dennisseaguirre3@gmail.com>
2021-08-07 14:45:05 -0500

    Correccion

--- Grupo_6/src/TDAs/Tree.java
+++ Grupo_6/src/TDAs/Tree.java
@@ -5,9 +5,6 @@
  */
 package TDAs;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Tree<T> {
     TreeNode<T> root;
 
@@ -25,48 +22,6 @@ public class Tree<T> {
     
     public boolean isEmpty() {
         return this.root == null;
-    }
-    
-     public Tree<T> getLeft() {
-        return this.root.getLeft();
-    }
-
-    public Tree<T> getRight() {
-        return this.root.getRight();
-    }
-    
-    public Tree(T contenido) {
-        this.root = new TreeNode<>(contenido);
-    }
-     public Tree() {
-        this.root = new TreeNode<>();
-    }
-    
-    public void largestValueOfEachLevelIterative(Tree<Integer> tree) {
-        if (tree == null || tree.isEmpty())
-            return;
-        Queue<Tree<Integer>> elementos = new LinkedList<>();
-        elementos.offer(tree);
-        int nivel = 1;
-        while (!elementos.isEmpty()){
-            int initialSize = elementos.size();
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < initialSize; i++) {
-               Tree<Integer> arbol = elementos.poll();
-               Tree<Integer> arbolizquierdo = arbol.getLeft(), arbolDerecho = arbol.getRight();
-               TreeNode<Integer> raiz = arbol.root;
-                if(raiz != null && max < raiz.getContenido())
-                    max = raiz.getContenido();
-
-                if (arbolizquierdo !=null)
-                    elementos.offer(arbolizquierdo);
-                if (arbolDerecho != null)
-                    elementos.offer(arbolDerecho);
-            }
-            System.out.println("Valor mayor es: " + nivel + ": " + max);
-            nivel++;
-        }
-
     }
     //Largest Value
     //find
--- Grupo_6/src/TDAs/TreeNode.java
+++ Grupo_6/src/TDAs/TreeNode.java
@@ -9,30 +9,20 @@ import java.util.List;
 
 public class TreeNode<T> {
     //Estado estado;
-    private T contenido;
+   
     TreeNode padre;
     List<TreeNode> hijos;
-    private Tree<T> left;
-    private Tree<T> right;
+    
     
     public TreeNode(TreeNode padre, List<TreeNode> hijos) {
         this.padre = padre;
         this.hijos = hijos;
     }
-    public TreeNode() {
-        this.contenido=null;
-        this.left=null;
-        this.right=null;
-    }
+    
     public TreeNode getPadre() {
         return padre;
     }
     
-     public TreeNode(T contenido) {
-        this.contenido=contenido;
-        left=null;
-        right=null;
-    }
 
     public List<TreeNode> getHijos() {
         return hijos;
@@ -47,17 +37,4 @@ public class TreeNode<T> {
     }
     
     
-    //Ver si gano
-
-    public T getContenido() {
-        return contenido;
-    }
-
-    public Tree<T> getLeft() {
-        return left;
-    }
-
-    public Tree<T> getRight() {
-        return right;
-    }
 }
